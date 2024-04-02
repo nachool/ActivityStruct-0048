660df54b3d7f327f398dcfb406ea48b95d4398d6
nachool <nachma.olivia1394@gmail.com>
2024-04-02 18:23:44 +0700

    menambah input int main

--- activity2-struct/activity2-struct.cpp
+++ activity2-struct/activity2-struct.cpp
@@ -15,6 +15,20 @@ struct Mahasiswa {
 
 int main()
 {
-    
+    Mahasiswa mhs; //akan mewakili struktur maasiswa
+    cout << "Nomor mahasiswa: ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa: ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa: ";
+    cin >> mhs.alamat;
+    cout << "Umur mahasiswa: ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM: " << mhs.nim;
+    cout << "\n Nama: " << mhs.nama;
+    cout << "\n Alamat: " << mhs.alamat;
+    cout << "\n Umur: " << mhs.umur;
 }
 
